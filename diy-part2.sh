a7bbecbc7d8cc83093e77eead2ef915f6605f94a
sleel <30924409+sleel@users.noreply.github.com>
2022-02-05 18:25:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i "s/OpenWrt /SEELE compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
