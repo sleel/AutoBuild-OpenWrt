655e0c6bfbcadac241eb9429a491f64d5f98e570
sleel <30924409+sleel@users.noreply.github.com>
2022-10-18 10:58:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,6 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 sed -i "s/OpenWrt /SEELE COMPILED ON $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
 sed -i "s/hostname='OpenWrt'/hostname='NERV'/g" ./package/base-files/files/bin/config_generate
+
+sed -i 's/bootstrap/argon-18.06/g' ./feeds/luci/collections/luci/Makefile
+rm -rf ./feeds/luci/luci-theme-argon
