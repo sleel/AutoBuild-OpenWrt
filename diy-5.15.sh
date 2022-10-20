63496f61848157be880fe12d1f15139c7ad04967
sleel <30924409+sleel@users.noreply.github.com>
2022-10-20 14:01:30 +0800

    Update diy-5.15.sh

--- diy-5.15.sh
+++ diy-5.15.sh
@@ -20,7 +20,6 @@ sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Make
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-#sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-#sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-
+sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
+sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
