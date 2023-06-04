b78bf77514c412e2c99079e863de28a583103135
sleel <30924409+sleel@users.noreply.github.com>
2023-06-04 20:12:53 +0800

    Update diy-6.1.sh

--- diy-6.1.sh
+++ diy-6.1.sh
@@ -17,6 +17,7 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makef
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 git clone https://github.com/linkease/istore.git package/istore
