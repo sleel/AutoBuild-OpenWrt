2d967ef9220b3b16d8c665af7493631dc5a802c0
sleel <30924409+sleel@users.noreply.github.com>
2024-06-27 20:17:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 sed -i "s/OpenWrt /SEELE COMPILED ON $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
 sed -i "s/hostname='OpenWrt'/hostname='NERV-SEELE'/g" ./package/base-files/files/bin/config_generate
+
+find ./ | grep Makefile | grep luci-app-unblockmusic | xargs rm -f
+rm -rf ./feeds/luci/applications/luci-app-unblockmusic
+rm -rf ./package/feeds/luci/luci-app-unblockmusic
