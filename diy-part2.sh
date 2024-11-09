3a79b67450d9412ec89bd06e7c3d79f846157dc4
sleel <30924409+sleel@users.noreply.github.com>
2024-11-09 15:14:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,4 +22,4 @@
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 sed -i "s/OpenWrt /SEELE COMPILED ON $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
-sed -i "s/hostname='OpenWrt'/hostname='NERV-SEELE'/g" ./package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/NERV-SEELE/g' package/base-files/files/bin/config_generate
