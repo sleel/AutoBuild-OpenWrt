1ee8aefe4fcb6949d5154fb151084f06ba4bf43f
sleel <30924409+sleel@users.noreply.github.com>
2025-01-16 14:48:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,9 +19,7 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-ZZZ="package/lean/default-settings/files/zzz-default-settings"
+ZZZZ="package/lean/default-settings/files/zzz-default-settings"
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
-sed -i "s/LEDE /SEELE COMPILED ON $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
-sed -i 's/OpenWrt/NERV-SEELE/g' package/base-files/files/bin/config_generate
-rm -rf ./feeds/packages/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
-curl -fsSL  https://raw.githubusercontent.com/sleel/AutoBuild-OpenWrt/refs/heads/main/update_cloudflare_com_v4.sh > ./feeds/packages/net/ddns-scripts/files/usr/lib/ddns/update_cloudflare_com_v4.sh
+sed -i "s/LEDE /SEELE COMPILED ON $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" $ZZZ
+sed -i "s/hostname='LEDE'/hostname='NERV'/g" ./package/base-files/files/bin/config_generate
