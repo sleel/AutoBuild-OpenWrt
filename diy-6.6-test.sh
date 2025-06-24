89665152eadffa78a06fb3b249512e08f20462d3
sleel <30924409+sleel@users.noreply.github.com>
2025-06-24 14:11:44 +0800

    Update diy-6.6-test.sh

--- diy-6.6-test.sh
+++ diy-6.6-test.sh
@@ -24,5 +24,3 @@ sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feed
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/luci-app-argon-config
