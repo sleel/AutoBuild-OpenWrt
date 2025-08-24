3b69de1260914e9b1327f15aaba82bc9f94b47be
sleel <30924409+sleel@users.noreply.github.com>
2025-08-24 17:31:30 +0800

    Update and rename diy-6.12-new.sh to diy-6.12-2410.sh

--- diy-6.12-2410.sh
+++ diy-6.12-2410.sh
@@ -11,11 +11,14 @@
 #
 
 # KERNEL_PATCHVER
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
+sed -i 's/^KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/' ./target/linux/x86/Makefile
 
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '/^src-git luci/d' feeds.conf.default
+sed -i '/^src-git helloworld/d' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -23,4 +26,6 @@ sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makef
 sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
 sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 sed -i '$a src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+git clone https://github.com/sleel/mypkgs.git tmp_mypkgs
+mv tmp_mypkgs/* package/ && rm -rf tmp_mypkgs
+sed -i 's|include ../../luci.mk|include $(TOPDIR)/feeds/luci/luci.mk|' package/luci-app-msd_lite/Makefile
