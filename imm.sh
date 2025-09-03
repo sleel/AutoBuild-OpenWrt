fd1358d466516dd918fb73f09c5474b1a88e5996
sleel <30924409+sleel@users.noreply.github.com>
2025-09-03 20:04:52 +0800

    Add files via upload

---
+++ imm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# KERNEL_PATCHVER
+#sed -i 's/^KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=5.10/' ./target/linux/ramips/Makefile
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/sleel/mypkgs.git tmp_mypkgs
+mv tmp_mypkgs/* package/ && rm -rf tmp_mypkgs
+sed -i 's|include ../../luci.mk|include $(TOPDIR)/feeds/luci/luci.mk|' package/luci-app-msd_lite/Makefile
