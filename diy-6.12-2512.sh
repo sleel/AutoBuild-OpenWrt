b27ad8ad313b5e732a558c2ddd3c198f5cab28ea
sleel <30924409+sleel@users.noreply.github.com>
2026-08-18 18:16:11 +0800

    Create new script file diy-6.12-2512.sh

--- diy-6.12-2512.sh
+++ diy-6.12-2512.sh
@@ -18,7 +18,7 @@ sed -i 's/^KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.12/' ./target/linux/x86/Makefi
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '/^src-git luci/d' feeds.conf.default
 sed -i '/^src-git helloworld/d' feeds.conf.default
-sed -i '$a src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10' feeds.conf.default
+sed -i '$a src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
